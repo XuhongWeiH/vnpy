4084c4a48f43f6ab6301a88387e9a801bcde0835
XuhongWeiH <big_weixuhong@qq.com>
2020-01-10 18:17:50 +0800

    testfirst

--- vnpy/app/algo_trading/__init__.py
+++ vnpy/app/algo_trading/__init__.py
@@ -5,7 +5,7 @@ from vnpy.trader.app import BaseApp
 from .engine import AlgoEngine, APP_NAME
 from .template import AlgoTemplate
 
-
+ 
 class AlgoTradingApp(BaseApp):
     """"""
 
@@ -13,6 +13,7 @@ class AlgoTradingApp(BaseApp):
     app_module = __module__
     app_path = Path(__file__).parent
     display_name = "算法交易"
+    
     engine_class = AlgoEngine
-    widget_name = "AlgoManager"
+    widget_name = "AlgoManager" 
     icon_name = "algo.ico"
